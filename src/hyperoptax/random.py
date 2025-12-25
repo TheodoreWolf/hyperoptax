a185a61ebf5845b33adb378e8584a63bba6e195d
Theo Wolf <theodore.wolf2000@gmail.com>
2025-12-25 14:54:28 +0100

    Working gridsearch

--- src/hyperoptax/random.py
+++ src/hyperoptax/random.py
@@ -1,10 +1,13 @@
+import logging
 from typing import Callable
 
 import jax
 import jax.numpy as jnp
 from flax import struct
 
-from hyperoptax.spaces import Space
+from hyperoptax import spaces as sp
+
+logger = logging.getLogger(__name__)
 
 
 @struct.dataclass
@@ -15,8 +18,7 @@ class OptimizerState:
 class Optimizer(struct.PyTreeNode):
     @classmethod
     def init(cls, space, n_seeds: int = 1) -> OptimizerState:
-        cls._init(space, n_seeds)
-        return OptimizerState(space=space)
+        return cls._init(space, n_seeds)
 
     @classmethod
     def _init(cls, space, n_seeds: int = 1):
@@ -48,6 +50,10 @@ class Optimizer(struct.PyTreeNode):
 
 class RandomSearch(Optimizer):
     @classmethod
+    def _init(cls, space, n_seeds):
+        return OptimizerState(space=space)
+
+    @classmethod
     def _optimize(
         cls,
         state: OptimizerState,
@@ -60,7 +66,88 @@ class RandomSearch(Optimizer):
         state, results = jax.vmap(cls._loop, in_axes=(None, 0, None, None, None))(
             state, keys, func, n_iterations, n_parallel
         )
-        results = results.reshape((n_iterations * n_parallel, -1))
+        results = jax.tree.map(
+            lambda x: x.reshape((n_iterations * n_parallel, -1)), results
+        )
+        return state, results
+
+    @classmethod
+    def _loop(
+        cls,
+        state: OptimizerState,
+        key: jax.random.PRNGKey,
+        func: Callable,
+        n_iterations: int,
+        n_parallel: int = 1,
+    ) -> tuple[OptimizerState, tuple[jax.Array, struct.PyTreeNode]]:
+        def _step(
+            carry: tuple[OptimizerState, jax.random.PRNGKey], _
+        ) -> tuple[OptimizerState, tuple[jax.Array, struct.PyTreeNode]]:
+            state, key = carry
+            key, subkey = jax.random.split(key)
+            tree = jax.tree_util.tree_structure(
+                state.space, is_leaf=lambda x: isinstance(x, sp.Space)
+            )
+            keys = jax.random.split(subkey, tree.num_leaves)
+            keys = jax.tree_util.tree_unflatten(tree, keys)
+            next_params = jax.tree.map(
+                lambda x, k: x.sample(k),
+                state.space,
+                keys,
+                is_leaf=lambda x: isinstance(x, sp.Space),
+            )
+            results = func(**next_params)
+
+            return (state, key), (results, next_params)
+
+        state, (results, params) = jax.lax.scan(
+            _step, (state, key), length=n_iterations
+        )
+        return state, (results, params)
+
+
+@struct.dataclass
+class GridSearchState:
+    space: struct.PyTreeNode
+    space_idx: struct.PyTreeNode
+    idx_pointer: int
+    random_shuffle = False
+
+
+class GridSearch(Optimizer):
+    @classmethod
+    def _init(cls, space, n_seed):
+        # assert the space is discrete for grid search
+        assert any(
+            jax.tree.flatten(
+                jax.tree.map(
+                    lambda x: isinstance(x, sp.DiscreteSpace),
+                    space,
+                    is_leaf=lambda x: isinstance(x, sp.Space),
+                ),
+            )[0]
+        ), "GridSearch with non-Discrete spaces is not possible."
+        space_idx = jax.tree.map(
+            lambda x: 0, space, is_leaf=lambda x: isinstance(x, sp.Space)
+        )
+        return GridSearchState(space, space_idx, 0)
+
+    @classmethod
+    def _optimize(
+        cls,
+        state: OptimizerState,
+        key: jax.random.PRNGKey,
+        func: Callable,
+        n_iterations: int,
+        n_parallel: int = 1,
+    ) -> tuple[OptimizerState, jax.Array]:
+        keys = jax.random.split(key, n_parallel)
+        state, results = jax.vmap(cls._loop, in_axes=(None, 0, None, None, None))(
+            state, keys, func, n_iterations, n_parallel
+        )
+        results = jax.tree.map(
+            lambda x: x.reshape((n_iterations * n_parallel, -1)), results
+        )
         return state, results
 
     @classmethod
@@ -73,22 +160,60 @@ class RandomSearch(Optimizer):
         n_parallel: int = 1,
     ) -> tuple[OptimizerState, jax.Array]:
         def _step(carry, _):
-            state, key = carry
+            key, state = carry
             key, subkey = jax.random.split(key)
             tree = jax.tree_util.tree_structure(
-                state.space, is_leaf=lambda x: isinstance(x, Space)
+                state.space, is_leaf=lambda x: isinstance(x, sp.Space)
             )
             keys = jax.random.split(subkey, tree.num_leaves)
             keys = jax.tree_util.tree_unflatten(tree, keys)
             next_params = jax.tree.map(
-                lambda x, k: x.sample(k),
+                lambda x, y: jnp.array(x.values).at[y].get(),
                 state.space,
-                keys,
-                is_leaf=lambda x: isinstance(x, Space),
+                state.space_idx,
+                is_leaf=lambda x: isinstance(x, sp.Space),
             )
             results = func(**next_params)
+            new_state = update_state(state)
 
-            return (state, key), results
+            return (key, new_state), (results, next_params)
 
-        state, results = jax.lax.scan(_step, (state, key), length=n_iterations)
-        return state, results
+        (key, state), (results, params) = jax.lax.scan(
+            _step, (key, state), length=n_iterations
+        )
+        return state, (results, params)
+
+
+def update_state(state: GridSearchState) -> GridSearchState:
+    # Flatten state.space_idx to get leaves + treedef
+    idx_leaves, idx_treedef = jax.tree_util.tree_flatten(state.space_idx)
+    space_leaves, _ = jax.tree_util.tree_flatten(
+        state.space, is_leaf=lambda x: isinstance(x, sp.Space)
+    )
+    N = len(idx_leaves)
+    idx_arr = jnp.stack(idx_leaves)
+    lengths = [len(x.values) for x in space_leaves]
+    lengths = jnp.array(lengths, dtype=idx_arr.dtype)  # shape (N,)
+    pointer = jnp.asarray(state.idx_pointer)
+    out_of_bounds = pointer >= N
+    cur_idx = jax.lax.dynamic_slice(idx_arr, (pointer,), (1,))[0]
+    max_idx_for_pointer = lengths[pointer] - 1
+
+    def return_unchanged():
+        return state
+
+    def handle_in_bounds():
+        def inc_pointer():
+            new_pointer = pointer + 1
+            return state.replace(idx_pointer=jnp.asarray(new_pointer))
+
+        def inc_idx():
+            new_idx_arr = idx_arr.at[pointer].add(1)
+            new_space_idx = jax.tree_util.tree_unflatten(idx_treedef, new_idx_arr)
+            return state.replace(space_idx=new_space_idx)
+
+        is_full = cur_idx == max_idx_for_pointer
+        return jax.lax.cond(is_full, inc_pointer, inc_idx)
+
+    new_state = jax.lax.cond(out_of_bounds, return_unchanged, handle_in_bounds)
+    return new_state
--- tests/test_random.py
+++ tests/test_random.py
@@ -1,7 +1,9 @@
 import jax
+import jax.numpy as jnp
+import pytest
 
-from hyperoptax.random import RandomSearch
 from hyperoptax import spaces as sp
+from hyperoptax.random import GridSearch, RandomSearch, update_state
 
 
 def test_random_search():
@@ -12,6 +14,44 @@ def test_random_search():
     key = jax.random.PRNGKey(0)
     state = RandomSearch.init(space)
     state, results = RandomSearch.optimize(state, key, _func, 100, 10)
-    assert results.shape == (1000, 1)
-    assert results.min() < 0.1
-    assert results.max() > 0.9
+    assert results[0].shape == (1000, 1)
+    assert results[0].min() < 0.1
+    assert results[0].max() > 0.9
+
+
+def test_grid_search_init():
+    def _func(x: float) -> float:
+        return x**2
+
+    space = {"x": sp.DiscreteSpace([0, 0.5, 1])}
+    state = GridSearch.init(space)
+    assert state.space_idx == {"x": jnp.zeros(1)}
+
+
+def test_grid_search_init_with_non_discrete_space():
+    space = {"x": sp.LinearSpace(0, 1)}
+    with pytest.raises(AssertionError):
+        GridSearch.init(space)
+
+
+def test_grid_search():
+    def _func(x: float) -> float:
+        return x**2
+
+    space = {"x": sp.DiscreteSpace([0, 0.5, 1])}
+    key = jax.random.PRNGKey(0)
+    state = GridSearch.init(space)
+    state, results = GridSearch.optimize(state, key, _func, 100, 10)
+    assert results[0].shape == (1000, 1)
+    assert results[0].min() == 0
+    assert results[0].max() == 1
+
+
+# def test_update_space_idx():
+#     space_idx = {"x": jnp.array([0]), "y": jnp.array([0]), "xy": {"z": jnp.array([0])}}
+#     space = {
+#         "x": sp.DiscreteSpace([0, 0.5, 1]),
+#         "y": sp.DiscreteSpace([1.0, 1.5, 2.0]),
+#         "xy": {"z": sp.DiscreteSpace([3.0, 3.5, 4.0])},
+#     }
+#     new_space_idx = update_state(space_idx, space, 0)
